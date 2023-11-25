3645702fc2aae0927748e60693ea1653bfb1ece5
wanding <weew9900@gmail.com>
2023-11-25 17:25:34 +0900

    first commit

---
+++ choco-filled/src/main/java/com/example/chocofilled/ChocoFilledApplication.java
@@ -0,0 +1,13 @@
+package com.example.chocofilled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChocoFilledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChocoFilledApplication.class, args);
+	}
+
+}
